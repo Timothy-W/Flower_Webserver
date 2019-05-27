f729202d410a4201778fd7b009972be0c32a412e
tim <timothy.l.walker1@gmail.com>
2019-05-26 18:37:56 -0700

    cleaned up code

--- savetofile.php
+++ savetofile.php
@@ -1,14 +1,9 @@
 <?php
 
-$data = "MRNG,";
-
 //Morning Tasks
 
-
-
-
 if(isset($_POST['mtask1']) && $_POST['mtask1'] != ""){
-        $data = $data . $_POST['mtask1'] . ',';
+        $data = $_POST['mtask1'] . ',';
 } 
 
 if (isset($_POST['mtask2']) && $_POST['mtask2'] != ""){
@@ -16,7 +11,7 @@ if (isset($_POST['mtask2']) && $_POST['mtask2'] != ""){
 }
 
 if(isset($_POST['mtask3']) && $_POST['mtask3'] != ""){
-        $data = $data . $_POST['mtask3'] . ',';
+        $data = $_POST['mtask3'] . ',';
 } 
 
 if (isset($_POST['mtask4']) && $_POST['mtask4'] != ""){
@@ -24,46 +19,44 @@ if (isset($_POST['mtask4']) && $_POST['mtask4'] != ""){
 }
 
 //Afternoon Tasks
-$data = $data . "AFTN,";
 
 
 if(isset($_POST['atask1']) && $_POST['atask1'] != ""){
-        $data = $data . $_POST['atask1'] . ',';
+        $data = $_POST['atask1'] . ',';
 } 
 
-if (isset($_POST['atask2']) && $_POST['atask2'] != ""){
-        $data = $data . $_POST['atask2'] . ',';
+if (isset($_POST['atask2']) && $_POST['mtask2'] != ""){
+        $data = $data . $_POST['mtask2'] . ',';
 }
 
-if(isset($_POST['atask3']) && $_POST['atask3'] != ""){
-        $data = $data . $_POST['atask3'] . ',';
+if(isset($_POST['atask3']) && $_POST['mtask3'] != ""){
+        $data = $_POST['mtask3'] . ',';
 } 
 
-if (isset($_POST['atask4']) && $_POST['atask4'] != ""){
-        $data = $data . $_POST['atask4'] . ',';
+if (isset($_POST['mtask4']) && $_POST['mtask4'] != ""){
+        $data = $data . $_POST['mtask4'] . ',';
 }
 
 //Evening Tasks
-$data = $data . "EVN,";
 
-if(isset($_POST['etask1']) && $_POST['etask1'] != ""){
-        $data = $data . $_POST['etask1'] . ',';
+
+if(isset($_POST['mtask1']) && $_POST['mtask1'] != ""){
+        $data = $_POST['mtask1'] . ',';
 } 
 
-if (isset($_POST['etask2']) && $_POST['etask2'] != ""){
-        $data = $data . $_POST['etask2'] . ',';
+if (isset($_POST['mtask2']) && $_POST['mtask2'] != ""){
+        $data = $data . $_POST['mtask2'] . ',';
 }
 
-if(isset($_POST['etask3']) && $_POST['etask3'] != ""){
-        $data = $data . $_POST['etask3'] . ',';
+if(isset($_POST['mtask3']) && $_POST['mtask3'] != ""){
+        $data = $_POST['mtask3'] . ',';
 } 
 
-if (isset($_POST['etask4']) && $_POST['etask4'] != ""){
-        $data = $data . $_POST['etask4'] . ',';
+if (isset($_POST['mtask4']) && $_POST['mtask4'] != ""){
+        $data = $data . $_POST['mtask4'] . ',';
 }
 
-$data = rtrim($data,',');
-echo "data: $data\r\n";
+echo "data: $data\n";
 
 if($data != ""){
 
