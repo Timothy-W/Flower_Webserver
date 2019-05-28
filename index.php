afb4ae536edf70e9f6d16d0a6560d75fc3595399
tim <timothy.l.walker1@gmail.com>
2019-05-28 15:50:16 -0700

    Added three tables, dropdown menu and add/delete task buttons

--- index.php
+++ index.php
@@ -1,34 +1,85 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8"/>
-        <title>Enter Tasks</title> 
-    </head>
+<html>
+<head>
+
+<style>
+table, td {
+  border: 0px solid black;
+}
+  
+  
+</style>
+</head>
 <body>
- 
-<p id="task_list"></p>
- 
-        <form action="savetofile.php" method="POST">
-           Morning Task 1: <input name="mtask1" type="text" value=""/><br/>
-           Morning Task 2: <input name="mtask2" type="text" value=""/><br/>
-           Morning Task 3: <input name="mtask3" type="text" value=""/><br/>
-           Morning Task 4: <input name="mtask4" type="text" value=""/><br/>
-           <br>
-           Afternoon Task 1: <input name="atask1" type="text" value=""/><br/>
-           Afternoon Task 2: <input name="atask2" type="text" value=""/><br/>
-           Afternoon Task 3: <input name="atask3" type="text" value=""/><br/>
-           Afternoon Task 4: <input name="atask4" type="text" value=""/><br/> 
-           <br>
-           Evening Task 1: <input name="etask1" type="text" value=""/><br/>
-           Evening Task 2: <input name="etask2" type="text" value=""/><br/>
-           Evening Task 3: <input name="etask3" type="text" value=""/><br/>
-           Evening Task 4: <input name="etask4" type="text" value=""/><br/> 
-           <input type="submit" name="submit" value="Save Tasks">
-        </form>
+
+<p>Select time of day: 
+<select id="timeOfDay" name="time">
+  <option value="Morning">Morning</option>
+  <option value="Afternoon">Afternoon</option>
+  <option value="Evening">Evening</option>
+</select>   
+</p>
+
+<form>
+  Enter task: <input id="task_input" name="task" type="text"/><br/>
+</form>
+
+<!-- <input type="submit" name="submit" value="Save Tasks"> -->
+<button onclick="myCreateFunction()">Add Task</button>
+<button onclick="myDeleteFunction()">Delete Last Task</button><br>  
+
+<!--
+<form action="javascript:myCreateFunction()" method="POST">
+  Enter task: <input id="task_input" name="task" type="text"/><br/>
+  <input type="submit" name="submit" value="Save Tasks">
+</form>  
+-->
+
+  
+<table id="Morning">
+ <th>Morning Tasks</th>
+</table>
+<br>
+  
+<table id="Afternoon">
+ <th>Afternoon Tasks</th>
+</table>
+<br>
+  
+<table id="Evening">
+ <th>Evening Tasks</th>
+</table>
+<br>
+
+
+<script type="text/javascript">
+function myCreateFunction() {
+  var task = document.getElementById("task_input").value;
+  var time = document.getElementById("timeOfDay").value;
+    
+  if (task != ""){
+    var table = document.getElementById(time);
+    var row = table.insertRow(-1);
+    var cell = row.insertCell(0);
+    cell.innerHTML = task;
+  }
+}
+
+function myDeleteFunction() {
+  
+  var time = document.getElementById("timeOfDay").value;
+   
+  var numCells = document.getElementById(time).rows.length;
+  
+  if (numCells > 1){
+  document.getElementById(time).deleteRow(-1);
+
+  }
+}
+  
+  
+  
+</script>
 
 </body>
 </html>
-
-
-
-
