6055af3599feb5878e2a56b6ce3a2acb4875c3d1
Timothy Walker <timothy.l.walker1@gmail.com>
2019-05-29 16:42:32 -0700

    Cleaned up savetofile.php code

--- savetofile.php
+++ savetofile.php
@@ -19,14 +19,6 @@ foreach ($_POST['Evening'] as $key => $value) {
  }
 
 
-
-
-
-
-
-
-
-
 echo "data: $data\n";
 
 if($data != ""){
@@ -43,44 +35,6 @@ if($ret === false) {
 else {
    die('No tasks entered');
 }
-
-
-
-
-
-
-
-
-
-
-
-/*
-if (isset($_POST['mtask4']) && $_POST['mtask4'] != ""){
-        $data = $data . $_POST['mtask4'] . ',';
-}
-
-echo "data: $data\n";
-
-if($data != ""){
-
-$ret = file_put_contents('/var/www/html/data/mydata.csv', $data, LOCK_EX);
-
-if($ret === false) {
-        die('There was an error writing this file');
-} else {
-        echo "$ret bytes written to file";
-        }
-
-}
-else {
-   die('No tasks entered');
-}
-
-
-
-
-*/
-
 
 
 ?>
