58a189eedecef594a8b6aa1a71e4e56ece4e7dd4
tim <timothy.l.walker1@gmail.com>
2019-05-28 18:02:45 -0700

    Changed nginx root directory in default; Added color to installer.sh

--- installer.sh
+++ installer.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
-#Bash script to install web server for flower reminder
-echo "Installing nginx and php.\n"
-sudo apt-get install nginx php-fpm
 
-echo "Copying over new default config file.\n"
+PURP='\033[0;35m'
+NC='\033[0m'
+
+#Bash script to install web server for flower reminder
+echo "${PURP}Installing nginx and php.${NC}"
+sudo apt install nginx php-fpm
+
+echo "${PURP}Copying over new default config file.${NC}"
 #cd /etc/nginx
 sudo cp -f $(pwd)/default /etc/nginx/sites-enabled/default
 
 #echo "Reloading config file.\n"
 #sudo /etc/init.d/nginx reload
 
-echo "Copying web interface files.\n"
+echo "${PURP}Copying web interface files.${NC}"
 #cd /var/www/html/
 #sudo cp -f $(pwd)/index.html /var/www/html/index.html
 #sudo cp -f $(pwd)/index.php /var/www/html/index.php
 #sudo cp -f $(pwd)/savetofile.php /var/www/html/savetofile.php
 
-echo "Create data dir and fix permissions"
+echo "${PURP}Creating data dir and fixing permissions${NC}"
 #sudo mkdir /var/www/html/data
 #sudo chmod 755 /var/www/html/data
 sudo mkdir $(pwd)/data
 sudo chmod 755 $(pwd)/data
 
-echo "Starting nginx service.\n"
+echo "${PURP}Starting nginx service.${NC}"
 #sudo /etc/init.d/nginx start
 sudo systemctl enable nginx
 sudo systemctl start nginx
 
-echo "\nNGINX default web page location: /var/www/html/index.nginx-debian.html\n"
-echo "Installation complete.\n"
-echo "Raspberry Pi IP Addresses: "
+echo "${PURP}NGINX default web page location: /var/www/html/index.nginx-debian.html${NC}"
+echo "${PURP}Installation complete.${NC}"
+echo "${PURP}Raspberry Pi IP Addresses: "
 hostname -I
