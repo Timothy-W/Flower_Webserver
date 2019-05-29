457a50321ed556dca648f569bba38d3e00ff84d3
Tim <timothy.l.walker1@gmail.com>
2019-05-29 08:31:08 +0100

    Finally working

--- index.php
+++ index.php
@@ -23,64 +23,75 @@ table, td {
 
 <!-- Enter Tasks -->
 <form>
-  Enter task: <input id="task_input" name="task" type="text"/>
+  Enter task: <input id="task_input" name="input_task" type="text"/>
 </form><br>
 
 <!-- Buttons -->
 <button onclick="myCreateFunction()">Add Task</button>
 <button onclick="myDeleteFunction()">Delete Last Task</button><br>
 
+  
+  
+  
+<!-- Table on display -->
+<table id="Morning2">
+ <th>Morning Tasks</th>
+</table>
+<br>
+  
+<table id="Afternoon2">
+ <th>Afternoon Tasks</th>
+</table>
+<br>
+  
+<table id="Evening2">
+ <th>Evening Tasks</th>
+</table>
+<br>
+
+
+
 <!-- Save all added tasks to file using php script -->
 <br><form action="savetofile.php" method="POST">
  <!-- Tables --> 
 <table id="Morning">
- <th>Morning Tasks</th>
+<th></th>
 </table>
 <br>
-  
+     
 <table id="Afternoon">
- <th>Afternoon Tasks</th>
+<th></th>
 </table>
 <br>
-  
-<table id="Evening">
- <th>Evening Tasks</th>
+     
+<table id="Evening">   
+<th></th>
 </table>
-<br>
+<br> 
   <input type="submit" name="taskTable" value="Save Tasks to Flower">
 </form><br>  
- 
-  
-  
-  
-<!-- Tables  
-<table id="Morning">
- <th>Morning Tasks</th>
-</table>
-<br>
-  
-<table id="Afternoon">
- <th>Afternoon Tasks</th>
-</table>
-<br>
-  
-<table id="Evening">
- <th>Evening Tasks</th>
-</table>
-<br>
--->
+
+
 
 
 <script type="text/javascript">
 function myCreateFunction() {
   var task = document.getElementById("task_input").value;
   var time = document.getElementById("timeOfDay").value;
-    
+  var cellString = "<input type='hidden' name='" + time + "[]'" + " value='" + task + "'/>";
+  
   if (task != ""){
     var table = document.getElementById(time);
     var row = table.insertRow(-1);
     var cell = row.insertCell(0);
+    cell.innerHTML = cellString;	
+
+    var table = document.getElementById(time + '2');
+    var row = table.insertRow(-1);
+    var cell = row.insertCell(0);
     cell.innerHTML = task;
+
+
   }
 }
 
@@ -92,6 +103,7 @@ function myDeleteFunction() {
   
   if (numCells > 1){
   document.getElementById(time).deleteRow(-1);
+  document.getElementById(time + '2').deleteRow(-1);
 
   }
 }
