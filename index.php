7e387ad6c77c064cb164f733cf88b25d271d22dd
Timothy Walker <timothy.l.walker1@gmail.com>
2019-05-29 20:35:17 -0700

    unknown changes

--- index.php
+++ index.php
@@ -107,10 +107,51 @@ function myDeleteFunction() {
 
   }
 }
-  
-  
-  
+    
 </script>
+
+
+<?php
+
+$data = "Morning,";
+
+foreach ($_POST['Morning'] as $key => $value) {
+        $data = $data . $value . ',';
+ }
+
+$data = $data . "Afternoon,";
+
+foreach ($_POST['Afternoon'] as $key => $value) {
+        $data = $data . $value . ',';
+ }
+
+$data = $data . "Evening,";
+
+foreach ($_POST['Evening'] as $key => $value) {
+        $data = $data . $value . ',';
+ }
+
+
+//echo "data: $data\n";
+
+if($data != ""){
+
+$ret = file_put_contents('/tmp/mydata.csv', $data, LOCK_EX);
+
+if($ret === false) {
+        die('There was an error writing this file');
+} else {
+        echo "$ret bytes written to file";
+        }
+
+}
+else {
+   die('No tasks entered');
+}
+
+
+?>
+
 
 </body>
 </html>
